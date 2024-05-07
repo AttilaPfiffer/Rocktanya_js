ab1792d3072b4ce1f2737461acd114b4140bc6eb
AttilaPfiffer <pifuati@gmail.com>
2024-05-07 17:23:33 +0200

    nev szures

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 import { polokLista } from "./adat.js";
-import { kartyakOsszeallitasa, kosarbaButton, megjelenitesKartyak } from "./fuggvenyek.js";
+import { kartyakOsszeallitasa, kosarbaButton, megjelenitesKartyak, szuresNevSzerint } from "./fuggvenyek.js";
 
 
+init(polokLista)
 
-megjelenitesKartyak(kartyakOsszeallitasa(polokLista))
+kosarbaButton(polokLista)
+nevSzuresEsemeny()
+
+function init() {
+    megjelenitesKartyak(kartyakOsszeallitasa(polokLista))
+
+}
 
 
+function nevSzuresEsemeny(){
+    const szuroELEM = $("#name");
+    szuroELEM.on("keyup", function(){
+        let szurtSzoveg = szuroELEM.val();
+        const LISTA = szuresNevSzerint(polokLista, szurtSzoveg);
+        init(LISTA)
+    });
+}
